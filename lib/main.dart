a2726ec10cab0534fa12732f1b8c6cfa4718fa72
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-08 17:28:53 -0400

    s04-76 Mapping data into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -48,9 +48,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
           ),
         ],
       ),
