5010968a344b236d99faa361b243ebd04b35b944
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-09 14:41:31 -0400

    s04-79 Styling Text

--- lib/main.dart
+++ lib/main.dart
@@ -59,13 +59,21 @@ class MyHomePage extends StatelessWidget {
                         horizontal: 15,
                       ),
                       decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         ),
                       ),
                       padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString()),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
                     ),
                     Column(
                       children: [
