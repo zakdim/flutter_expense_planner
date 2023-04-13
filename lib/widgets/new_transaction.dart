b53f0d7620d8dea115e6b3b2824807f1a36752c5
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-12 21:30:52 -0400

    s04-361 Improving & styling the modal botton sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTxHandler;
 
   NewTransaction(this.addTxHandler);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
@@ -15,7 +22,8 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addTxHandler(enteredTitle, enteredAmount);
+    widget.addTxHandler(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
   }
 
   @override
